67549ef182626077c5d12bbf9c86511c6abd4401
Nate Tolbert <ntolbertu85@gmail.com>
2021-04-27 03:45:21 -0400

    04272021 -- Project/Global -- INITIAL COMMIT - Project freshly extracted from Initializr;

---
+++ src/main/java/com/mdspicesmicro/kitchenservice/MdS11KitchenServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mdspicesmicro.kitchenservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdS11KitchenServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdS11KitchenServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mdspicesmicro/kitchenservice/MdS11KitchenServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdspicesmicro.kitchenservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdS11KitchenServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
